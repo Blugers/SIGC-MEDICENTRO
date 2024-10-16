a35920107bcda81c7204a81db3854155d81a8165
Blugers <github.blugers@gmail.com>
2024-10-16 11:15:39 -0500

    -Commit subida de proyecto

---
+++ CapaEntidad/ContratoLaboral.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class ContratoLaboral
+    {
+        int idContrato, idTipoContrato, idTrabajador, horasTotales;
+        string descripcionContrato;
+        bool estadoContrato;
+        DateTime fechaInicioContrato, fechaFinContrato, fechaRegistroContrato;
+
+        public int IdContrato { get => idContrato; set => idContrato = value; }
+        public int IdTipoContrato { get => idTipoContrato; set => idTipoContrato = value; }
+        public int IdTrabajador { get => idTrabajador; set => idTrabajador = value; }
+        public int HorasTotales { get => horasTotales; set => horasTotales = value; }
+        public string DescripcionContrato { get => descripcionContrato; set => descripcionContrato = value; }
+        public bool EstadoContrato { get => estadoContrato; set => estadoContrato = value; }
+        public DateTime FechaInicioContrato { get => fechaInicioContrato; set => fechaInicioContrato = value; }
+        public DateTime FechaFinContrato { get => fechaFinContrato; set => fechaFinContrato = value; }
+        public DateTime FechaRegistroContrato { get => fechaRegistroContrato; set => fechaRegistroContrato = value; }
+
+        public ContratoLaboral()
+        {
+
+        }
+    }
+}
---
+++ CapaEntidad/TipoContratoLaboral.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class TipoContratoLaboral
+    {
+        int idTipoContrato;
+        string nombreTipoContrato;
+        decimal salarioBaseTipoContrato;
+        DateTime fechaRegistroTipoContrato;
+        bool estadoTipoContrato;
+
+        public int IdTipoContrato { get => idTipoContrato; set => idTipoContrato = value; }
+        public string NombreTipoContrato { get => nombreTipoContrato; set => nombreTipoContrato = value; }
+        public decimal SalarioBaseTipoContrato { get => salarioBaseTipoContrato; set => salarioBaseTipoContrato = value; }
+        public DateTime FechaRegistroTipoContrato { get => fechaRegistroTipoContrato; set => fechaRegistroTipoContrato = value; }
+        public bool EstadoTipoContrato { get => estadoTipoContrato; set => estadoTipoContrato = value; }
+
+        public TipoContratoLaboral()
+        {
+
+        }
+    }
+}
